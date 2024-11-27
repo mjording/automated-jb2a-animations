cdd401e2885ee253393a1db22465344ee397268a
Matthew Jording <mjording@gmail.com>
2024-11-27 00:54:35 -0500

    adding support for unofficial marvel multiverse rpg

---
+++ src/system-support/aa-marvel-multiverse.js
@@ -0,0 +1,40 @@
+import { debug }            from "../constants/constants.js";
+import { trafficCop }       from "../router/traffic-cop.js";
+import AAHandler            from "../system-handlers/workflow-data.js";
+import { getRequiredData }  from "./getRequiredData.js";
+
+// DnD5e System hooks provided to run animations
+export function systemHooks() {
+
+    Hooks.on("marvel-multiverse.rollAttack", async (item, roll) => {
+        let playOnDamage = game.settings.get('autoanimations', 'playonDamageCore')
+        if (item.hasDamage && playOnDamage) { return; }   
+        attack(await getRequiredData({item, actor: item.actor, workflow: item}))
+    })
+    Hooks.on("marvel-multiverse.calcDamage", async (item, roll) => {
+        let playOnDamage = game.settings.get('autoanimations', 'playonDamageCore')
+        if (item.hasAttack && !playOnDamage) { return; }
+        damage(await getRequiredData({item, actor: item.actor, workflow: item}))
+    })
+
+}
+
+/**
+ * 
+ * @param {Boolean} hasAttack // Checks if the item has Attack
+ * @param {Boolean} hasDamage // Checks if the item has Damage
+ *  
+ */
+
+async function attack(input) {
+    debug("Attack rolled, checking for animations");
+    const handler = await AAHandler.make(input)
+    if (!handler?.item || !handler?.sourceToken) { console.log("Automated Animations: No Item or Source Token", handler); return;}
+    trafficCop(handler)
+}
+async function damage(input) {
+    debug("Damage ready, checking for animations")
+    const handler = await AAHandler.make(input)
+    if (!handler?.item || !handler?.sourceToken) { console.log("Automated Animations: No Item or Source Token", handler); return;}
+    trafficCop(handler)
+}
